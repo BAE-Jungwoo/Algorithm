49ca7c62d579aa913d206d357e8f185869407da8
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-12-01 16:10:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	int dp[1001] = { 0, };
+	dp[1] = 1;
+	dp[2] = 3;
+
+	cin >> n;
+
+	// 맨 오른쪽에 1x2 타일 1개를 세로로 놓는 경우 → dp[i - 1]개
+    // 맨 오른쪽에 2x1 타일 2개를 가로로 놓는 경우 → dp[i - 2]개
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+	cout << dp[n] << "\n";
+	return 0;
+}
