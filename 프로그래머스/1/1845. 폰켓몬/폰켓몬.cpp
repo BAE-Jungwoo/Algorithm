d41f928c0d67eb97f7e5bbd8d4376786dedcb5dd
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-02 20:41:49 +0900

    [level 1] Title: 폰켓몬, Time: 2.09 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int solution(vector<int> nums)
 {
     int answer = 0;
-    set<int> s;
-    for(int i = 0; i < nums.size(); i++) 
-        s.insert(nums[i]);
+    set<int> s(nums.begin(), nums.end());
     if (s.size() <= (nums.size() / 2)) answer = s.size();
     else if (s.size() > (nums.size() / 2)) answer = nums.size() / 2;
     return answer;
