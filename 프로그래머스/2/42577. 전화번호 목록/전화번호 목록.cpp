c6f3771fc9a0d60c77d141ff5ddd9a054302caaf
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-15 00:29:56 +0900

    [level 2] Title: 전화번호 목록, Time: 145.92 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -2,23 +2,18 @@
 #include <vector>
 #include <set>
 #include <algorithm>
-
 using namespace std;
 
 bool solution(vector<string> phone_book)
 {
     bool answer = true;
     set<string> book(phone_book.begin(), phone_book.end());
-    for (auto &iter : phone_book)
-        book.insert(iter);
 
-    while (book.size() > 1)
-    {
+    while (book.size() > 1) {
         auto iter = book.begin();
         string temp = *iter;
         iter = book.erase(iter);
-        if (iter->compare(0, temp.size(), temp) == 0)
-        {
+        if (iter->compare(0, temp.size(), temp) == 0) {
             answer = false;
             return answer;
         }
