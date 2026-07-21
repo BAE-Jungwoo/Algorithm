2fff8ccb6c7823962262cd238511e7ba4cf981f1
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-21 17:29:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.25 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 bool visited[101][101] = { false, };
 int dist[101][101] = { 0, };
+int dy[4] = { 0, 0, -1, 1 };    // 상, 하
+int dx[4] = { -1, 1, 0, 0 };    // 좌, 우
 
 // N x M 크기의 맵
 int solution(vector<vector<int> > maps)
 {
-    int sN = maps.size(), sM = maps[0].size(); // 행, 열
-    int dy[4] = { 0, 0, -1, 1 };    // 상, 하
-    int dx[4] = { -1, 1, 0, 0 };    // 좌, 우
-
-    vector<vector<int>> map(sN + 1, vector<int>(sM + 1));
-    for (int i = 1; i <= sN; i++) {
-        for (int j = 1; j <= sM; j++) {
+    // (1, 1)에서 시작하기 위해
+    vector<vector<int>> map(maps.size() + 1, vector<int>(maps[0].size() + 1));
+    for (int i = 1; i <= maps.size(); i++) {
+        for (int j = 1; j <= maps[0].size(); j++) {
             map[i][j] = maps[i - 1][j - 1];
         }
     }
