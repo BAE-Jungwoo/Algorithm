84183ea106ee4edd242eb12b5664fa553da3c6db
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-21 17:21:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.28 ms, Memory: 4.51 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+bool visited[101][101] = { false, };
+int dist[101][101] = { 0, };
+
+// N x M 크기의 맵
+int solution(vector<vector<int> > maps)
+{
+    int sN = maps.size(), sM = maps[0].size(); // 행, 열
+    int dy[4] = { 0, 0, -1, 1 };    // 상, 하
+    int dx[4] = { -1, 1, 0, 0 };    // 좌, 우
+
+    vector<vector<int>> map(sN + 1, vector<int>(sM + 1));
+    for (int i = 1; i <= sN; i++) {
+        for (int j = 1; j <= sM; j++) {
+            map[i][j] = maps[i - 1][j - 1];
+        }
+    }
+
+    int N = map.size(), M = map[0].size(); // 행, 열
+    queue<pair<int, int>> path;
+    path.push({ 1, 1 }); // 시작점 (y, x)
+    visited[1][1] = true;
+    dist[1][1] = 1;
+
+    while (!path.empty()) {
+        pair<int, int> temp = path.front();
+        path.pop();
+
+        for (int i = 0; i < 4; i++) {   // 좌, 우, 상, 하
+            int ny = temp.first + dy[i];
+            int nx = temp.second + dx[i];
+
+            if (nx >= 1 && nx < M && ny >= 1 && ny < N) {
+                if (map[ny][nx] == 1 && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    path.push({ ny, nx });
+                    dist[ny][nx] = dist[temp.first][temp.second] + 1;
+
+                    if (ny == N - 1 && nx == M - 1)
+                        return dist[ny][nx];
+                }
+            }
+        }
+    }
+    return -1;
+}
