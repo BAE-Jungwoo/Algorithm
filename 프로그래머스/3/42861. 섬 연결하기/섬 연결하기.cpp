0375975e362597a656fbe8f1669b2e27d29c9ffd
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-26 20:54:36 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,54 +1,46 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#define INT_MAX 987654321
 using namespace std;
 
-// x 정점의 부모 정점 찾기
-int getRoot(vector<int>& parent, int x) {
-    if (parent[x] == x) 
-        return x;
-    return parent[x] = getRoot(parent, parent[x]);
-}
-
-// 부모가 같은, 같은 그룹으로 두 정점을 병합
-void unionParent(vector<int>& parent, int a, int b) {
-    int parent_a = getRoot(parent, a);
-    int parent_b = getRoot(parent, b);
-    if (parent_a < parent_b)
-        parent[parent_b] = parent_a;
-    else
-        parent[parent_a] = parent_b;
-}
-
-// 두 정점이 같은 부모를 가졌는지 확인
-bool find(vector<int>& parent, int a, int b) {
-    int parent_a = getRoot(parent, a);
-    int parent_b = getRoot(parent, b);
-    if (parent[parent_a] == parent[parent_b])
-        return true;
-    else
-        return false;
-}
-
-// 비용 오름차순 정렬
-bool compare(vector<int>& a, vector<int>& b) {
-    return a[2] < b[2];
-}
 
 int solution(int n, vector<vector<int>> costs) {
-    int answer = 0;   
-    sort(costs.begin(), costs.end(), compare);
-
-    vector<int> parent(n);
-    for (int i = 0; i < parent.size(); ++i) 
-        parent[i] = i;
-    
-    for (int i = 0; i < costs.size();++i) {
-        if (!find(parent, costs[i][0], costs[i][1])) {
-            unionParent(parent, costs[i][0], costs[i][1]);
-            answer += costs[i][2];
-        }
+    int answer = 0;
+    vector<vector<int>> graph(n, vector<int>(n));
+    for (int i = 0; i < costs.size(); i++)
+    {
+        graph[costs[i][0]][costs[i][1]] = costs[i][2];
+        graph[costs[i][1]][costs[i][0]] = costs[i][2];
     }
+
+    vector<int> visited;
+    vector<int> unvisited;
+
+	visited.push_back(0);
+	for (int i = 1; i < n; i++)
+		unvisited.push_back(i);
+
+	for (int i = 1; i < n; i++)
+	{
+		int min = INT_MAX;
+		int min_index = 0;
+
+		for (int j = 0; j < i; j++)
+		{
+			for (int k = 0; k < n - i; k++)
+			{
+				if (graph[visited[j]][unvisited[k]] > 0 && min > graph[visited[j]][unvisited[k]])
+				{
+					min = graph[visited[j]][unvisited[k]];
+					min_index = k;
+				}
+			}
+		}
+
+		visited.push_back(unvisited[min_index]);
+		unvisited.erase(unvisited.begin() + min_index);
+		answer += min;
+	}
 
     return answer;
 }
