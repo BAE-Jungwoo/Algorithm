6c2c10e24ce902c6e57da74425cc03efebe0e572
배정우 <113417378+BAE-Jungwoo@users.noreply.github.com>
2026-07-28 10:31:59 +0900

    Add Kruskal solution for Connecting Island

---
+++ 프로그래머스/3/42861. 섬 연결하기/Connecting_Island_Kruskal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// x 정점의 부모 정점 찾기
+int getRoot(vector<int>& parent, int x) {
+    if (parent[x] == x) 
+        return x;
+    return parent[x] = getRoot(parent, parent[x]);
+}
+
+// 부모가 같은, 같은 그룹으로 두 정점을 병합
+void unionParent(vector<int>& parent, int a, int b) {
+    int parent_a = getRoot(parent, a);
+    int parent_b = getRoot(parent, b);
+    if (parent_a < parent_b)
+        parent[parent_b] = parent_a;
+    else
+        parent[parent_a] = parent_b;
+}
+
+// 두 정점이 같은 부모를 가졌는지 확인
+bool find(vector<int>& parent, int a, int b) {
+    int parent_a = getRoot(parent, a);
+    int parent_b = getRoot(parent, b);
+    if (parent[parent_a] == parent[parent_b])
+        return true;
+    else
+        return false;
+}
+
+// 비용 오름차순 정렬
+bool compare(vector<int>& a, vector<int>& b) {
+    return a[2] < b[2];
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;   
+    sort(costs.begin(), costs.end(), compare);
+
+    vector<int> parent(n);
+    for (int i = 0; i < parent.size(); ++i) 
+        parent[i] = i;
+    
+    for (int i = 0; i < costs.size(); ++i) {
+        // 부모가 같다면 사이클을 형성하므로, 부모가 같지 않을때만
+        if (!find(parent, costs[i][0], costs[i][1])) {
+            unionParent(parent, costs[i][0], costs[i][1]);
+            answer += costs[i][2];
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    int n = 4;
+    vector<vector<int>> costs = { {0,1,1}, {0,2,2}, {1,2,5}, {1,3,1}, {2,3,8} };
+    int result = solution(n, costs);
+    cout << "result : " << result;
+    return 0;
+}
